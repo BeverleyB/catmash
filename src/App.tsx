ab63fc6ba32c2070040ccb1b88e64804f91acc04
BeverleyB <beverley.beranger@gmail.com>
2024-03-11 20:24:23 +0100

    Add routing for navigate between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Vote from './pages/Vote/vote';
+import Result from './pages/Result/result';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>COUCOU CATMASH</h1>
-    </div>
-  );
+
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Vote />} />
+				<Route path="/result" element={<Result />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
