4cc0294ed2253d0c1b5c41aa6cd8fbbc4b5ad34f
BeverleyB <beverley.beranger@gmail.com>
2024-03-12 18:57:50 +0100

    update service to add random vote on each cat

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
-import Cats from '../interfaces/cats';
+import Cat from '../interfaces/cats';
 
-export default async function get_cats_data(): Promise<Cats[] | boolean> {
+export default async function get_cats_data(): Promise<Cat[] | boolean> {
     try {
 		const req = await axios.get(`https://data.latelier.co/cats.json`);
-		return (req.data.images);
+		// Add random vote on each cat
+		const newReqWithVote = req.data.images.map((cat: Cat) => ({...cat, vote: Math.floor(Math.random() * 1000)}));
+		return (newReqWithVote);
 	}
 	catch (e) {
+		console.error('No data founds, please contact support.');
 		return (false);
 	}
 }
