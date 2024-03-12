84646b9567721da6bc5b4feb757e9aa1265a732f
BeverleyB <beverley.beranger@gmail.com>
2024-03-12 18:59:21 +0100

    add function vote to the cutest cat

--- src/pages/Vote/vote.tsx
+++ src/pages/Vote/vote.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Cats from '../../interfaces/cats';
+import Cat from '../../interfaces/cats';
 import get_cats_data from '../../services/services';
 
 import './vote.scss';
 
 function Vote() {
-	const [_Cats, set_Cats] = React.useState<Cats[]>([]);
+	const [_Cats, set_Cats] = React.useState<Cat[]>([]);
 
 	React.useEffect(() => {
 		get_pair_of_cats();
@@ -20,11 +20,19 @@ function Vote() {
 		set_Cats(randomCats.slice(0, 2));
 	}
 
+	function vote_for_cute_cat(cat: Cat) {
+		// Increase the vote number by 1 in the imaginary db :)
+		set_Cats(_Cats.map(newCat => {
+			return newCat.id === cat.id ? { ...newCat, vote: newCat.vote += 1 } : newCat;
+		}));
+
+		get_pair_of_cats();
+	}
 
 	return (
 		<div id="vote">
-			{ _Cats.map((cat: Cats) => 
-				<img key={cat.id} src={cat.url} alt="Cat" />
+			{ _Cats.map((cat: Cat, index: number) => 
+				<img key={cat.id} src={cat.url} alt="Cat" onClick={() => vote_for_cute_cat(cat)}/>
 			)}
 		</div>
 	);
