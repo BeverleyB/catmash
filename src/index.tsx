d1b5dbd47e979f781785edd798ff4c5e30c845dd
BeverleyB <beverley.beranger@gmail.com>
2024-03-13 12:49:03 +0100

    delete strict mode because useEffect reload twice

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+import './styles/global.scss';
+import './styles/fonts.scss';
+
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
 
