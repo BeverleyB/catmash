b20043e59c049492a8f407f91b6e218445b503d5
BeverleyB <beverley.beranger@gmail.com>
2024-03-13 14:16:58 +0100

    Fix router because it didn't work with github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 import './styles/global.scss';
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<BrowserRouter>
+	<Router basename={"/"}>
 		<App />
-	</BrowserRouter>
+	</Router>
 );
 
