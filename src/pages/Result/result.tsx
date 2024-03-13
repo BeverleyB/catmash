cdf6e1aebb6bf9d8f33a75e4e2f60c849cb29478
BeverleyB <beverley.beranger@gmail.com>
2024-03-13 10:07:36 +0100

    add load more button

--- src/pages/Result/result.tsx
+++ src/pages/Result/result.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 
-import Cats from '../../interfaces/cats';
+import Cat from '../../interfaces/cats';
 import get_cats_data from '../../services/services';
 
+import './result.scss';
+
 function Result() {
-	const [_Cats, set_Cats] = React.useState<Cats[]>([]);
+	const [_Cats, set_Cats] = React.useState<Cat[]>([]);
+	const [_CatsToShow, set_CatsItemsToShow] = React.useState<number>(3);
 
 	React.useEffect(() => {
-		async function get_all_cats() {
-			const cats = await get_cats_data();
-			if (typeof cats !== 'boolean') set_Cats(cats);
-		}
-
 		get_all_cats();
 	}, []);
 
+	async function get_all_cats() {
+		const cats = await get_cats_data();
+		if (typeof cats !== 'boolean') {
+			// Sort array by vote
+			cats.sort((a, b) => b.vote - a.vote);
+			set_Cats(cats);
+		}
+	}
+
 	return (
 		<div id="result">
-			{ _Cats.map((cat: Cats) => 
-				<img key={cat.id} src={cat.url} alt="Cat" />
+
+			<div className="container-logo">
+				<div className="logo" />
+				<h1>Le podium</h1>
+			</div>
+
+
+			{ _Cats.slice(0, _CatsToShow).map((cat: Cat, index: number) => 
+				<div className="container-cat" key={cat.id}>
+					<h2 className="ranked">#{index + 1}</h2>
+					<img className='cat-pic' src={cat.url} alt="Cat" />
+					<div className="number-vote">{cat.vote} vote{cat.vote > 1 ? 's': ''}</div>
+				</div>
+			)}
+
+			{_CatsToShow < _Cats.length && (
+				<button onClick={() => set_CatsItemsToShow(_CatsToShow + 10)}>Load More</button>
 			)}
 		</div>
 	);
