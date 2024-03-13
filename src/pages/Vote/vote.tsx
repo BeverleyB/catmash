1ec052eb6e5d4986063fa08627d47515ca4f9a13
BeverleyB <beverley.beranger@gmail.com>
2024-03-13 12:47:51 +0100

    fix html vote page and add style for this page

--- src/pages/Vote/vote.tsx
+++ src/pages/Vote/vote.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Cat from '../../interfaces/cats';
 import get_cats_data from '../../services/services';
@@ -7,6 +8,9 @@ import './vote.scss';
 
 function Vote() {
 	const [_Cats, set_Cats] = React.useState<Cat[]>([]);
+	const [_NumberVotes, set_NumberVotes] = React.useState<number>(Math.floor(Math.random() * 5000));
+
+	const navigate = useNavigate();
 
 	React.useEffect(() => {
 		get_pair_of_cats();
@@ -25,15 +29,32 @@ function Vote() {
 		set_Cats(_Cats.map(newCat => {
 			return newCat.id === cat.id ? { ...newCat, vote: newCat.vote += 1 } : newCat;
 		}));
+		set_NumberVotes(_NumberVotes + 1)
 
 		get_pair_of_cats();
 	}
 
 	return (
 		<div id="vote">
-			{ _Cats.map((cat: Cat, index: number) => 
-				<img key={cat.id} src={cat.url} alt="Cat" onClick={() => vote_for_cute_cat(cat)}/>
-			)}
+
+			<div className="container-logo">
+				<div className="logo" />
+				<h1>CATMASH</h1>
+			</div>
+			
+
+			<div className="cats-pictures">
+				{ _Cats.map((cat: Cat) => 
+				<div className="img-container" key={cat.id} onClick={() => vote_for_cute_cat(cat)}>
+					<img src={cat.url} alt="Cat"/>
+				</div>
+				)}
+			</div>
+
+			<button className="btn" type="button" onClick={() => navigate('/result')}>
+				Voir les plus beaux chats <br />
+				<span className='vote-result'>{_NumberVotes} votes</span>
+			</button>
 		</div>
 	);
 }
